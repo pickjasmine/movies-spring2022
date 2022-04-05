f065fd2c9f79820961663995d680b47c9008f921
Jasmine Francois <francois.jasminem@gmail.com>
2022-04-04 20:54:38 -0500

    Showing how to use .map to loop over array of objects

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import {movies} from './movies';
 const App = () => {
     return (
         <div>
-            <p>Jasmine Francois</p>
-            <pre>{JSON.stringify(movies, null, 4)}</pre>
+            {/* The following .map() is equivalent to the 10 <p> lines below */}
+
+            {
+                movies.map((movie) => <p>{movie.title}</p>)
+            }
+
+            <p>{movies[0].title}</p>
+            <p>{movies[1].title}</p>
+            <p>{movies[2].title}</p>
+            <p>{movies[3].title}</p>
+            <p>{movies[4].title}</p>
+            <p>{movies[5].title}</p>
+            <p>{movies[6].title}</p>
+            <p>{movies[7].title}</p>
+            <p>{movies[8].title}</p>
+            <p>{movies[9].title}</p>
         </div>
     );
 }
