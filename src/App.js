0864543ddf95b8bfa4a3e21d75bed589f295a70c
Jasmine Francois <francois.jasminem@gmail.com>
2022-04-11 19:28:45 -0500

    Debugging error for returning multiple data points when mapping

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import {movies} from './movies';
 const App = () => {
     return (
         <div>
-            {/* The following .map() is equivalent to the 10 <p> lines below */}
-
+            {/* Error: Adjacent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</> */}
             {
-                movies.map((movie) => <p>{movie.title}</p>)
+                movies.map((movie) =>
+                    <>
+                        <p>{movie.title}</p>
+                        <img src={movie.posterUrl}/>
+                    </>
+                )
             }
-
-            <p>{movies[0].title}</p>
-            <p>{movies[1].title}</p>
-            <p>{movies[2].title}</p>
-            <p>{movies[3].title}</p>
-            <p>{movies[4].title}</p>
-            <p>{movies[5].title}</p>
-            <p>{movies[6].title}</p>
-            <p>{movies[7].title}</p>
-            <p>{movies[8].title}</p>
-            <p>{movies[9].title}</p>
         </div>
     );
 }
