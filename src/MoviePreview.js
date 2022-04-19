7825b8c93ec90985b4f3be0ce173dc5dcbe4d163
Jasmine Francois <francois.jasminem@gmail.com>
2022-04-18 19:30:30 -0500

    Saving state in MoviePreview, notes on calling functions inline

--- src/MoviePreview.js
+++ src/MoviePreview.js
@@ -1,15 +1,32 @@
 import './App.css';
+import {useState} from "react";
 
 const MoviePreview = (props) => {
     const {movieToPreview, setCurrentMovie} = props;
+    const [clickCount, setClickCount] = useState(0);
+
+    const updateMovieSelection = () => {
+        setCurrentMovie(movieToPreview);
+        setClickCount(clickCount + 1);
+    };
 
     return (
         <div>
             <p>{movieToPreview.title}</p>
+            <p>{clickCount}</p>
             <img
                 src={movieToPreview.posterUrl}
                 alt={movieToPreview.title}
-                onClick={() => setCurrentMovie(movieToPreview)}
+                onClick={updateMovieSelection}
+                {/*
+                    this is how you can do what is happening in this function
+                    but in-line with your HTML/react components
+
+                    onClick={() => {
+                        setCurrentMovie(movieToPreview);
+                        setClickCount(clickCount + 1);
+                    }}
+                */}
             />
         </div>
     )
